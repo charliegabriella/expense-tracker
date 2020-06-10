b79f5f32eda3e1de5c7226c68f8b7c7224414cd6
Charlie Kroon <c.kroon@ksyos.nl>
2020-06-10 19:32:58 +0200

    Create IncomeExpense and TransactionList components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Header } from "./components/Header"
 import { Balance } from "./components/Balance"
 import { IncomeExpenses } from "./components/IncomeExpenses"
+import { TransactionList } from "./components/TransactionList"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <div className="container"> 
           <Balance /> 
           <IncomeExpenses />
+          <TransactionList />
         </div>
 
     </>
