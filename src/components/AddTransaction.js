866fb89e4aae1af65ae98b5d4a86ecac2301fa39
Charlie Kroon <c.kroon@ksyos.nl>
2020-06-10 22:13:23 +0200

    Add formatting

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -10,15 +10,27 @@ export const AddTransaction = () => {
         <form className="form">
           <div className="form-control">
             <label htmlFor="text">Text</label>
-              <input type="text" value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..." />
+                <input 
+                type="text" 
+                value={text} 
+                onChange={(e) => setText(e.target.value)} 
+                id="text" 
+                placeholder="Enter text..." 
+                />
             </div>
             <div className="htmlForm-control">
-              <label HtmlFor="amount">
-                Amount <br />
-              (negative - expense, positive - income)</label
-            >
-            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" placeholder="Enter amount..." /> 
-          </div>
+              <label HtmlFor="amount">Amount 
+              <br />
+              (negative - expense, positive - income)
+              </label>
+                <input 
+                type="number" 
+                value={amount} 
+                onChange={(e) => setAmount(e.target.value)} 
+                id="amount" 
+                placeholder="Enter amount..." 
+                /> 
+            </div>
           <button className="btn">Add transaction</button>
         </form>
     </>
