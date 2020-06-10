ad0c7764bb88d8a778f774c5d60632f4982109d5
Charlie Kroon <c.kroon@ksyos.nl>
2020-06-10 21:32:16 +0200

    Create Transaction Component

---
+++ src/components/Transaction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Transaction = () => {
+    return (
+        <li className="minus">
+            {transaction.text}
+            <span>-€400</span>
+             <button className="delete-btn">x</button>
+        </li> 
+    )
+}
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Transaction } from './Transaction'
+import { GlobalContext } from "../context/GlobalState"
 
 export const TransactionList = () => {
-  return (
+    const { transactions } = useContext(GlobalContext);
+    
+    
+    return (
     <>
       <h3>History</h3>
         <ul id="list" className="list">
-          <li className="minus">Cash <span>-€400</span>
-          <button className="delete-btn">x</button>
-          </li> 
+            {transactions.map(transaction => (<Transaction transaction={transaction}/>))}  
         </ul>
     </>
   )
 }
+
+//Map of transactions: we're bringing it down from the global state (globalcontext). For each one we're going to render a transaction component, and we're passing in a prop. 
