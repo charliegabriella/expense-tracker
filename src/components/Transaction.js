752257b2db843ec3c1330c4a53d369bf7a22a84f
Charlie Kroon <c.kroon@ksyos.nl>
2020-06-10 22:02:46 +0200

    Adding props to Transaction. Adding Key to transaction component

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-export const Transaction = () => {
+export const Transaction = ({ transaction }) => {
+    const sign = transaction.amount < 0 ? '-' : '+';
+
     return (
-        <li className="minus">
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.text}
-            <span>-€400</span>
+            <span>{sign}€{Math.abs(transaction.amount)}</span> 
              <button className="delete-btn">x</button>
         </li> 
     )
 }
+
+//By using Math.abs, the amount is made an absolute number, which will always be positive.
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -5,15 +5,15 @@ import { GlobalContext } from "../context/GlobalState"
 export const TransactionList = () => {
     const { transactions } = useContext(GlobalContext);
     
-    
     return (
-    <>
-      <h3>History</h3>
-        <ul id="list" className="list">
-            {transactions.map(transaction => (<Transaction transaction={transaction}/>))}  
-        </ul>
-    </>
-  )
+        <>
+            <h3>History</h3>
+                <ul id="list" className="list">
+                    {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/>))}  
+                </ul>
+        </>
+    )
 }
 
 //Map of transactions: we're bringing it down from the global state (globalcontext). For each one we're going to render a transaction component, and we're passing in a prop. 
+//because the prop transaction is a list (look at initialState in globalcontext), we need to  use a key and for that key we need to use an id, because it needs to be unique. 
