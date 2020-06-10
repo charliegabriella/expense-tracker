f7af7a72a14752bbd894525d1898a63389f143db
Charlie Kroon <c.kroon@ksyos.nl>
2020-06-10 19:22:24 +0200

    Add IncomeExpense Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import { Header } from "./components/Header"
 import { Balance } from "./components/Balance"
+import { IncomeExpenses } from "./components/IncomeExpenses"
+
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-        <div className="container"> <Balance /> </div>
-    </div>
+        <div className="container"> 
+          <Balance /> 
+          <IncomeExpenses />
+        </div>
+
+    </>
   );
 }
 
